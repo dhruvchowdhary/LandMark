a962ca9158aade43f346f2a01aae8d93f35680eb
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2023-04-22 00:28:38 -0700

    Initial upload

---
+++ Compass/Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Rohan Taneja on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
