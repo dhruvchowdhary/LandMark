67840b21d82067aa010c123143d44bb726fc6b06
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2023-04-22 20:39:38 -0700

    home view set

--- Compass/Compass/HomeView.swift
+++ Compass/Compass/HomeView.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreMotion
 
 struct HomeView: View {
     @State private var tabBarHeight: CGFloat = 0
     @State private var tabBarOffset: CGFloat = UIScreen.main.bounds.height * 0.55 // Initial position
-    @State private var showPtImages = UserDefaults.standard.array(forKey: "showPtImages") ?? [false, false, false, false, false, false, false]
-    @State private var nextImageToShowIndex = UserDefaults.standard.integer(forKey: "nextImageToShowIndex") ?? 0
+    @State private var showPtImages = [false, false, false, false, false, false, false]
+    // UserDefaults.standard.array(forKey: "showPtImages") ??
+    @State private var nextImageToShowIndex = 0
+    // UserDefaults.standard.integer(forKey: "nextImageToShowIndex") ??
     @State private var inputText = ""
+    
+    @ObservedObject var compassHeading = CompassHeading()
+
+    @State private var setLocation: CLLocationCoordinate2D?
+    @State private var setLatitude: Double = 0
+    @State private var setLongitude: Double = 0
+    @State private var currLatitude: Double = 0
+    @State private var currLongitude: Double = 0
+    @State private var userHeading: Double = 0.0
+    @State private var angleFromNorth: Double = 0.0
+    @State private var arrowAngle: Double = 0.0
+    @State private var recentLocations: Array = []
+    @State private var counter: Int = 0
 
     
     var body: some View {
@@ -30,7 +47,7 @@ struct HomeView: View {
                         .resizable()
                         .scaledToFit()
                         .scaleEffect(0.2)
-                        .rotationEffect(Angle(degrees: 1))
+                        .rotationEffect(Angle(degrees: 1+angleFromNorth))
                         .offset(x: 0, y: 42.5)
                     if showPtImages[0] as! Bool {
                                             Image("pt0")
@@ -38,7 +55,7 @@ struct HomeView: View {
                                                 .scaledToFit()
                                                 .scaleEffect(0.1)
                                                 .offset(x: 0, y: -115-17-55/2)
-                                                .rotationEffect(Angle(degrees: 0))
+                                                .rotationEffect(Angle(degrees: arrowAngle))
                                                 .offset(x: 0, y: 17+55/2)
                                         }
                     if showPtImages[1] as! Bool {
@@ -102,6 +119,7 @@ struct HomeView: View {
                                 nextImageToShowIndex += 1
                                 UserDefaults.standard.set(showPtImages, forKey: "showPtImages")
                                 UserDefaults.standard.set(nextImageToShowIndex, forKey: "nextImageToShowIndex")
+                        getLocation()
                             } else {
                                 print("Error: index out of bounds")
                             }
@@ -117,6 +135,9 @@ struct HomeView: View {
             }
             .animation(.easeInOut(duration: 0.2))
             .offset(y: tabBarOffset - UIScreen.main.bounds.height * 0.55) // To adjust the view offset
+            .onAppear {
+                updateLocation()
+            }
             
             VStack {
                 VStack {
@@ -165,4 +186,82 @@ struct HomeView: View {
             }
         }
     }
+    
+    func getLocation() {
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            setLocation = locationManager.location?.coordinate
+            setLatitude = setLocation?.latitude ?? 0.0
+            setLongitude = setLocation?.longitude ?? 0.0
+            UserDefaults.standard.set(setLatitude, forKey: "setLatitude")
+            UserDefaults.standard.set(setLongitude, forKey: "setLongitude")
+//            currLocation = locationManager.location?.coordinate
+//            currLatitude = Double(UserDefaults.standard.float(forKey: "setLatitude"))
+//            currLongitude = Double(UserDefaults.standard.float(forKey: "setLongitude"))
+            print("Latitude: \(setLatitude), Longitude: \(setLongitude)")
+        }
+    }
+    
+    func updateLocation() {
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+//            if CLLocationManager.locationServicesEnabled() {
+                let currLocation = locationManager.location?.coordinate
+                currLatitude = currLocation?.latitude ?? 0.0
+                currLongitude = currLocation?.longitude ?? 0.0
+                
+                angleFromNorth = self.compassHeading.degrees * -1
+
+                let bearing = angleBetweenTwoLocations(fromLat: currLatitude, fromLong: currLongitude, toLat: setLatitude, toLong: setLongitude)
+                
+                let theta = bearing - angleFromNorth
+                if theta < 0 {
+                    arrowAngle = theta + 360
+                } else {
+                    arrowAngle = theta
+                }
+//            }
+            
+            counter += 1
+            if counter % 5 == 0 {
+                recentLocations.append((currLatitude, currLongitude))
+            }
+        }
+        timer.fire()
+    }
+    
+    func angleBetweenTwoLocations(fromLat: Double, fromLong: Double, toLat: Double, toLong: Double) -> Double {
+        let fromLatRad = fromLat.degreesToRadians
+        let fromLongRad = fromLong.degreesToRadians
+
+        let toLatRad = toLat.degreesToRadians
+        let toLongRad = toLong.degreesToRadians
+
+        let dLon = toLongRad - fromLongRad
+
+        let y = sin(dLon) * cos(toLatRad)
+        let x = cos(fromLatRad) * sin(toLatRad) - sin(fromLatRad) * cos(toLatRad) * cos(dLon)
+
+        let degBearing = atan2(y, x).radiansToDegrees
+        
+        if (degBearing >= 0) {
+            return degBearing
+        } else {
+            return 360 + degBearing
+        }
+    }
 }
+
+private extension Double {
+    var degreesToRadians: Double { return self * (.pi / 180) }
+    var radiansToDegrees: Double { return self * (180 / .pi) }
+}
+
